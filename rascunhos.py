609e262f5f14380cfe01a5f34fb684955adeaeaf
Nick <nicollivsantana@gmail.com>
2022-11-16 18:52:00 -0300

    Rascunhos atualizado

--- rascunhos.py
+++ rascunhos.py
@@ -4,11 +4,48 @@
 #s = n1+n2
 #print('A soma é {}!'.format(s))
 
+#CALCULADORA
+
+#n1 = float(input('Insira o primeiro valor:\n'))
+#n2 = float(input('Insira o segundo valor:\n'))
+#op = input('insira o tipo de operação:\n')
+
+#if op == '+':
+#    soma = n1+n2
+#    print('A soma entre {} e {} é {}'.format(n1,n2,soma))
+#elif op == '-':
+#     sub = n1-n2 
+#     print('A subtração entre {} e {} é {}'.format(n1,n2,sub))
+#elif op == "*":
+#    mult = n1*n2
+#    print('A multiplicação entre {} e {} é {}'.format(n1,n2,mult))
+#elif op == "/":
+#    div = n1/n2
+#    print('A divisão entre {} e {} é {}'.format(n1,n2,div))
+#elif op == "//":
+#    divex = n1//n2
+#    print('A divisão exata entre {} e {} é {}'.format(n1,n2,divex))
+#elif op == "**":
+#    pot = n1**n2
+#    print('{}, elevado a {} é {}'.format(n1,n2,pot))
+#elif op == "%":
+#    rest = n1%n2
+#    print('O resto da divisão entre {0} e {1} é {2}'.format(n1,n2,rest))
+
 #MEXENDO COM VARIAVEIS
 #var1 = 'azul'
 #var2 = 'vermelho'
 #var3 = 'rosa'
 #var4 = 'verde'
+
+#FAZER UM SAQUE
+
+#saldo = 500
+#saque = float(input('Insira o valor do saque desejado:\n'))
+#while saque>saldo:
+#    saque = float(input('Saldo insuficiente, insira um novo valor:\n'))
+#novosaldo = saldo-saque
+#print('Saque realizado com sucesso! Seu novo saldo é {}.'.format(novosaldo))
 
 #print('A flor é {0}, o carro é {1}, o céu é {2} e a árvore é {3}.'.format(var3,var2,var1,var4))
 
@@ -67,3 +104,13 @@
 #    c=c+1
 #print(lista_tarefas)
 
+#BRIMCANDO COM TUPLAS
+#Tuplas podem servir como elementos que armazenam outros elementos para uma lista
+
+#cliente1 = ('nick', '15', '000000000')
+#cliente2 = ('lua', '18', '111111111')
+#cliente3 = ('sol', '12', '222222222')
+
+#clientes = []
+#clientes.append(cliente1, cliente2, cliente3)
+#print(clientes[0])
