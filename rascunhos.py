c1c44f94a827fad01e21a0d2dc36ef717e4a93fa
Nick <nicollivsantana@gmail.com>
2022-11-16 18:01:43 -0300

    rascunhos atualizado até listas

--- rascunhos.py
+++ rascunhos.py
@@ -1,2 +1,69 @@
-print("hello, mundo!")
-print("I'm a new line!")
+# SOMA SIMPLES
+#n1 = int(input('Digite o primeiro número: '))
+#n2 = int(input('Digite o segundo número: '))
+#s = n1+n2
+#print('A soma é {}!'.format(s))
+
+#MEXENDO COM VARIAVEIS
+#var1 = 'azul'
+#var2 = 'vermelho'
+#var3 = 'rosa'
+#var4 = 'verde'
+
+#print('A flor é {0}, o carro é {1}, o céu é {2} e a árvore é {3}.'.format(var3,var2,var1,var4))
+
+#BRINCANDO COMCONDIÇÕES
+#Diz o nome, idade atrevez do ano e as próximas idades até 2100
+
+#name = input('Olá, qual é o seu nome?\n')
+#year1 = int(input('Certo {}, qual o ano em que você nasceu?\n' .format(name)))
+#year2 = int(input('Ok {}, agora me diga, em que ano estamos agora?\n' .format(name)))
+#age = year2-year1
+#if age>=18:
+#    print('Uau, você é bem grande {}! '.format(name) + 'Já tem {} anos e é maior de idade!'.format(age))
+#else:
+#    print('Wow,você ainda é bem jovenm {}! '.format(name) + 'Ainda tem {} anos e é menor de idade!'.format(age))
+
+#print('Veja a seguir suas próximas idades até 2100:')
+#contador = 0
+#while contador<79: 
+#    print(age+contador)
+#    contador = contador+1
+
+#BRINCANDO COM LISTAS
+
+#VER O ITEM DE ALGUMA LISTA PELA ORDEM:
+#lista_compras = ['banana', 'maça','uva', 'pera', 'melancia', 'morango', 'abacaxi', 'melão', 'laranja']
+#print(lista_compras[0])
+
+# -1 = EXIBE O ULTIMO ITEM
+
+# .APPEND ACRESCENTA ALGO NA UKTIMA POSIÇÃO
+#lista_compras = ['banana', 'maça','uva', 'pera', 'melancia', 'morango', 'abacaxi', 'melão', 'laranja']
+#lista_compras.append('limão')
+#print(lista_compras)
+
+# .INSERT ACRESCENTA NA POSIÇÃO DESEJADA
+#lista_compras = ['banana', 'maça','uva', 'pera', 'melancia', 'morango', 'abacaxi', 'melão', 'laranja']
+#lista_compras.insert(0, 'limão')
+#print(lista_compras)
+
+# DEL DELETA NA POSIÇÃO DESEJADA
+#lista_compras = ['banana', 'maça','uva', 'pera', 'melancia', 'morango', 'abacaxi', 'melão', 'laranja']
+#del lista_compras[1]
+#print(lista_compras)
+
+# REMOVE DELETA PELO NOME
+#lista_compras = ['banana', 'maça','uva', 'pera', 'melancia', 'morango', 'abacaxi', 'melão', 'laranja']
+#lista_compras.remove('abacaxi')
+#print(lista_compras)
+
+#PARA O USUARIO INSERIR O VALOR DA LISTA
+#lista_tarefas = []
+#c=0
+#while c<5:
+#    tarefa = input('Insira uma tarefa:\n')
+#    lista_tarefas.append(tarefa)
+#    c=c+1
+#print(lista_tarefas)
+
